41a02595c4edc2e46596d4c317e014665522cd27
Salomón Sarria <69904705+salo1802@users.noreply.github.com>
2021-12-01 01:34:11 -0500

    añadi autenticación

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,11 +1,12 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyD2NCb3ren0ZUB-YQ-kOcc2XKQL3Caf3R0",
-  authDomain: "eco-actividad10.firebaseapp.com",
-  databaseURL: "https://eco-actividad10-default-rtdb.firebaseio.com",
-  projectId: "eco-actividad10",
-  storageBucket: "eco-actividad10.appspot.com",
-  messagingSenderId: "402032649270",
-  appId: "1:402032649270:web:d073835a30d590ebc62497"
+    apiKey: "AIzaSyDvr5AZEdnbRmTQuKvph5cIeO1o662mTQ8",
+  authDomain: "ecosistemas-de-apps-2de1b.firebaseapp.com",
+  databaseURL: "https://ecosistemas-de-apps-2de1b-default-rtdb.firebaseio.com",
+  projectId: "ecosistemas-de-apps-2de1b",
+  storageBucket: "ecosistemas-de-apps-2de1b.appspot.com",
+  messagingSenderId: "131410176852",
+  appId: "1:131410176852:web:dce2237f75e75d0a53b6c1",
+  measurementId: "G-1KDBVWY3NV"
 };
 
 export function getFirebaseConfig(){
--- src/login.js
+++ src/login.js
@@ -1,12 +1,32 @@
+import {getFirebaseConfig} from './firebase-config.js';
+import { initializeApp } from 'firebase/app';
+import {getAuth, signInWithEmailAndPassword, onAuthStateChanged} from 'firebase/auth';
+
+const firebaseConf = getFirebaseConfig();
+const app = initializeApp(firebaseConf);
+const auth = getAuth();
 
 const user= document.getElementById('username');
 const password = document.getElementById('password');
 const logBtn = document.getElementById('loginBtn');
-const userID
+const userID;
+const localstorage = window.localStorage;
 
 logBtn.addEventListener("click",function(){
-    alert(user.value + " " + password.value);
-    const localstorage = window.localStorage;
-    localstorage.setItem('id', userID);
-    window.location.href="index.html";
+    signInWithEmailAndPassword(auth, user.value, password.value)
+    .then((userCredentials)=>{
+        if(user.value=="salo1802@hotmail.com"){userID= "user1"}
+        if(user.value=="davidm@gmail.com"){userID= "user2"}
+        if(user.value=="pepito123@gmail.com"){userID= "user3"}
+        if(user.value=="usuarigenerico@gmail.com"){userID="user4"}
+        localstorage.setItem('id', userID);
+        window.location.href="index.html";
+    })
+    .catch((error)=>{
+        console.log(error.message);
+
+    });
+
+    
+ 
 });
